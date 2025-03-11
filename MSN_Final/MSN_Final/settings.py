ae37c570b40abc3eabe586e3a25301d89ceb3655
Daniel409-tch <74939963+Daniel409-tch@users.noreply.github.com>
2025-03-10 20:02:18 -0500

    Modificacion de settings BD y installapp

--- MSN_Final/MSN_Final/settings.py
+++ MSN_Final/MSN_Final/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'administrador',
+    'cliente',
+    'mecanico',
 ]
 
 MIDDLEWARE = [
@@ -77,8 +80,12 @@ WSGI_APPLICATION = 'MSN_Final.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'modelofisico',
+        'USER':'root',
+        'PASSWORD':'',
+        'HOST':'localhost',
+        'PORT':'3306',
     }
 }
 
