c6d6bd5a02316f4d81a32721c7cc5a211bfdd8ed
Daniel409-tch <74939963+Daniel409-tch@users.noreply.github.com>
2025-03-11 15:24:05 -0500

    actualizaciones

--- MSN_Final/administrador/urls.py
+++ MSN_Final/administrador/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('insertarMecanico/', views.insertarMecanico, name='insertarMecanico'),
-    path('mecanicos/', views.mecanicos, name='mecanicos'),
-    path('mantenimientos/', views.mantenimientos, name='mantenimientos'),
-    path('historialesVehiculo/', views.historialesVehiculo, name='historialesVehiculo'),
-    path('talleresMecanico/', views.talleresMecanico, name='talleresMecanico'),
-    path('peritajes/', views.peritajes, name='peritajes'),
+    #path('insertarMecanico/', views.insertarMecanico, name='insertarMecanico'),
+    #path('mecanicos/', views.mecanicos, name='mecanicos'),
+    #path('mantenimientos/', views.mantenimientos, name='mantenimientos'),
+    #path('historialesVehiculo/', views.historialesVehiculo, name='historialesVehiculo'),
+    #path('talleresMecanico/', views.talleresMecanico, name='talleresMecanico'),
+    #path('peritajes/', views.peritajes, name='peritajes'),
 ]
