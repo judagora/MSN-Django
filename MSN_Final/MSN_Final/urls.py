78818e9af64d7ca0c6eca0096395cd2afe971b63
Juan David <159961071+JPP8457@users.noreply.github.com>
2025-03-15 15:17:05 -0500

    Vistas administrador casi terminadas

--- MSN_Final/MSN_Final/urls.py
+++ MSN_Final/MSN_Final/urls.py
@@ -1,10 +1,8 @@
 
-from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('administrador.urls')),
+    path('administrador/', include('administrador.urls')),
     path('', include('inicio.urls')),
     path('cliente/', include('cliente.urls')),
 ]
--- MSN_Final/administrador/views.py
+++ MSN_Final/administrador/views.py
@@ -1,3 +1,41 @@
 from django.shortcuts import render
 
 # Create your views here.
+def inicio (request):
+    return render(request, 'indexAdministrador.html')
+
+def insertarTaller (request):
+    return render(request, 'insertarTaller.html')
+
+def insertarMantenimiento (request):
+    return render(request, 'insertarMantenimiento.html')
+
+def modificarMecanico (request):
+    return render(request, 'modificarMecanico.html')
+
+def insertarMecanico (request):
+    return render(request, 'insertarMecanico.html')
+
+def mantenimientos (request):
+    return render(request, 'mantenimientos.html')
+
+def historialesVehiculo (request):
+    return render(request, 'historialesVehiculo.html')
+
+def talleresMecanico (request):
+    return render(request, 'talleresMecanico.html')
+
+def mecanicos (request):
+    return render(request, 'mecanicos.html')
+
+def peritajes (request):
+    return render(request, 'peritajes.html')
+
+def modificarPeritaje (request):
+    return render(request, 'modificarPeritaje.html')
+
+def modificarTaller (request):
+    return render(request, 'modificarTaller.html')
+
+def modificarMantenimiento (request):
+    return render(request, 'modificarMantenimiento.html')
