6e2a34f4e511ae6f19cf1104ff9d739cf57df920
judagora <jdgr2005397@gmail.com>
2025-03-15 21:43:09 -0500

    templates mecanico

--- MSN_Final/mecanico/urls.py
+++ MSN_Final/mecanico/urls.py
@@ -5,5 +5,22 @@ app_name = 'mecanico'
 
 urlpatterns = [
     path('inicio/', views.inicio, name='inicio'),
+    path('insertarMantenimiento/', views.insertarMantenimiento, name='insertarMantenimiento'),
+    path('modificarMantenimiento/', views.modificarMantenimiento, name='modificarMantenimiento'),
+    path('modificarMantenimiento2/', views.modificarMantenimiento2, name='modificarMantenimiento'),
+    path('consultarMantenimiento/', views.consultarMantenimiento, name='consultarMantenimiento'),
+    path('consultarMantenimiento2/', views.consultarMantenimiento2, name='consultarMantenimiento2'),
+    
+    path('insertarPeritaje/', views.insertarPeritaje, name='insertarPeritaje'),
+    path('modificarPeritaje/', views.modificarPeritaje, name='modificarPeritaje'),
+    path('modificarPeritaje2/', views.modificarPeritaje2, name='modificarPeritaje2'),
+    path('consultarPeritaje/', views.consultarPeritaje, name='consultarPeritaje'),
+    path('consultarPeritaje2/', views.consultarPeritaje2, name='consultarPeritaje2'),
+    
+    path('insertarRepuesto/', views.insertarRepuesto, name='insertarRepuesto'),
+    path('modificarRepuesto/', views.modificarRepuesto, name='modificarRepuesto'),
+    path('modificarRepuesto2/', views.modificarRepuesto2, name='modificarRepuesto2'),
+    path('consultarRepuesto/', views.consultarRepuesto, name='consultarRepuesto'),
+    path('consultarRepuesto2/', views.consultarRepuesto2, name='consultarRepuesto2'),
 ]
 
--- MSN_Final/mecanico/views.py
+++ MSN_Final/mecanico/views.py
@@ -2,3 +2,49 @@ from django.shortcuts import render
 
 def inicio(request):
     return render(request, 'indexMecanico.html')
+
+def insertarMantenimiento (request):
+    return render(request, 'insertarMantenimiento.html')
+
+def modificarMantenimiento (request):
+    return render(request, 'modificarMantenimiento.html')
+
+def modificarMantenimiento2 (request):
+    return render(request, 'modificarMantenimiento2.html')
+
+def consultarMantenimiento (request):
+    return render(request, 'consultarMantenimiento.html')
+
+def consultarMantenimiento2 (request):
+    return render(request, 'consultarMantenimiento2.html')
+
+def insertarPeritaje (request):
+    return render(request, 'insertarPeritaje.html')
+
+def modificarPeritaje (request):
+    return render(request, 'modificarPeritaje.html')
+
+def modificarPeritaje2 (request):
+    return render(request, 'modificarPeritaje2.html')
+
+def consultarPeritaje (request):
+    return render(request, 'consultarPeritaje.html')
+
+def consultarPeritaje2 (request):
+    return render(request, 'consultarPeritaje2.html')
+
+def insertarRepuesto (request):
+    return render(request, 'insertarRepuesto.html')
+
+def modificarRepuesto (request):
+    return render(request, 'modificarRepuesto.html')
+
+def modificarRepuesto2 (request):
+    return render(request, 'modificarRepuesto2.html')
+
+def consultarRepuesto (request):
+    return render(request, 'consultarRepuesto.html')
+
+def consultarRepuesto2 (request):
+    return render(request, 'consultarRepuesto2.html')
+
