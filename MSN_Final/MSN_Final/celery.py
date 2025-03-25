4a56595bd45491e89855126f7fb237329cda7d72
Daniel409-tch <74939963+Daniel409-tch@users.noreply.github.com>
2025-03-24 19:55:38 -0500

    update sin celery

--- MSN_Final/MSN_Final/celery.py
+++
@@ -1,7 +0,0 @@
-from celery import Celery
-import os
-import billiard as multiprocessing
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MSN_Final.settings')
-app = Celery('MSN_Final')
-app.config_from_object('django.conf:settings', namespace='CELERY')
-app.autodiscover_tasks()
